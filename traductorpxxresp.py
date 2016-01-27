02a2f1bf039dca9f6079f73922d8aabad1c95cf9
Francisco Carrillo Pérez <carrilloperezfrancisco@gmail.com>
2016-01-27 14:24:16 +0100

    First commit

---
+++ traductorpxxresp.py
@@ -0,0 +1,88 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+#       Copyright 2015 Francisco Carrillo (https://github.com/pacocp)
+#
+#       This program is free software: you can redistribute it and/or modify
+#       it under the terms of the GNU General Public License as published by
+#       the Free Software Foundation, either version 3 of the License, or
+#       (at your option) any later version.
+#
+#       This program is distributed in the hope that it will be useful,
+#       but WITHOUT ANY WARRANTY; without even the implied warranty of
+#       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#       GNU General Public License for more details.
+#
+#       You should have received a copy of the GNU General Public License
+#       along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+###############################################################################
+#
+#      Traductor de Pxxr Gvng - Español
+#
+###############################################################################
+
+#DICCIONARIO CON TODAS LAS PALABRAS, toda la información viene de este hilo de forocoches
+#http://www.forocoches.com/foro/showthread.php?t=4121257
+
+diccionario = {9: "Una pistola de 9mm",
+"bogos": "chico guapo",
+"Bogos": "Chico guapo",
+"balin": "Vivir vida manejando billetes",
+"Balin": "Vivir la vida manejando billetes",
+"booty": "culo",
+"Booty": "Culo",
+"bowlong": "fumar marihuana",
+"Bowlong": "Fumar marihuana",
+"bricks": "paquetes de droga",
+"Bricks": "Paquetes de droga",
+"buyate": "culo",
+"Buyate": "Culo",
+"canelo": "pringado",
+"Canelo": "Pringado",
+"cipollo": "tonto",
+"Cipollo": "Tonto",
+"clica": "grupo de colegas",
+"Clica": "Grupo de colegas",
+"clicka": "grupo de colegas",
+"Clicka": "Grupo de colegas",
+"coco": "cocaína",
+"Coco": "Cocaína",
+"cuira": "buena mierda",
+"Cuira": "Buena mierda",
+"djin": "genio",
+"Djin": "Genio",
+"dradis": "chavales",
+"Dradis": "Chavales",
+"dzebi": "polla",
+"Dzebi": "Polla",
+"esquiar": "esnifar",
+"Esquiar": "Esnifar",
+"feka": "pringado",
+"Feka": "Pringado",
+"feos": "policías",
+"Feos": "Policías",
+"flex": "presumir",
+"Flex": "Presumir",
+"flush": "dinero",
+"Flush": "Dinero",
+"G": "gansta",
+"Gabra": "Cocaína",
+"gabra": "cocaína",
+"goler": "esnifar",
+"Goler": "Esnifar",
+"goofy": "pringado",
+"Goofy": "Pringado",
+"guero": "barrio",
+"Guero": "Barrio",
+"guillao": "subido",
+"Guillao": "Subido",
+"habibi": "cariño",
+"Habibi": "Cariño",
+"hash": "hachís",
+"Hash": "Hachís",
+
+
+}
+
+#############################################################################
